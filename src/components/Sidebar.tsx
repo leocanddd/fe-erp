18329733e8cabcd4721d0b34c1462cffc0af664a
leocanddd <leocandraseldi@gmail.com>
2025-10-27 09:31:54 +0700

    p

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -303,7 +303,10 @@ export default function Sidebar({
 		{
 			name: 'Blogs',
 			href: '/blogs',
-			roles: [5], // Only Superadmin
+			roles: [
+				0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+				10, 11, 12,
+			], // Only Superadmin
 			icon: (
 				<svg
 					className="w-5 h-5"
